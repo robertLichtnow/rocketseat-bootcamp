fe12a1efdbff619be615dbe64df9ebe9bf7b2104
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-19 19:14:25 -0300

    Added nodemon and users array

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const server = express();
 
-// localhost:3000/teste
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+const users = ["Diego", "Claudio", "Victor"];
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
 });
 
 server.listen(3000);

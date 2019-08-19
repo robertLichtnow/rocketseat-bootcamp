d58530f0cd186e8f4d6d5aba4f0a68a25c5f4570
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-19 19:25:36 -0300

    Adding crud

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,11 +2,42 @@ const express = require("express");
 
 const server = express();
 
-const users = ["Diego", "Claudio", "Victor"];
+server.use(express.json());
+
+const users = ["Diego", "Robson", "Victor"];
+
+// CRUD - Create, Read, Update and Delete
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { name } = req.body;
+  const { index } = req.params;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);

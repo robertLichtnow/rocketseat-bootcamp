d87e45e054bceb6ab3783db6393bd416d632865c
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-23 22:39:58 -0300

    Adding user validation and hashing on password

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandez',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '1231231231223',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;

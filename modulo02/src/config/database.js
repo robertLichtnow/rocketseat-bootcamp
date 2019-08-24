13d2e1c7551d98144d6a057e8291323402248f15
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-23 21:41:32 -0300

    Added users migration and fixed database connection

--- modulo02/src/config/database.js
+++ modulo02/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'password',
+  password: 'postgres',
   database: 'gobarber',
   define: {
     timestamps: true,

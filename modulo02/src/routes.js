dbae539e68617628088cfb23baa97f2829d559cd
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-23 21:54:58 -0300

    Adding connection to database an testing user creation

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Teste' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandez',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1231231231223',
+  });
+
+  res.json(user);
 });
 
 export default routes;

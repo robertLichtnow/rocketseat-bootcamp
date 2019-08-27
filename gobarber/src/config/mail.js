842f2ebee0c3391ec62ccfe58ada7315b3d328a3
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-27 00:07:44 -0300

    Added mailing to canceled appointment

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e19b1511e4f559',
+    pass: 'a6ad202b472e4f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};

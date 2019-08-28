60abb19abd2c1e173eff2f8aebf310bfacd805d8
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-27 23:42:13 -0300

    Added new virtual attributes to appointments

--- gobarber/src/app/models/Appointment.js
+++ gobarber/src/app/models/Appointment.js
@@ -1,11 +1,26 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
+    const hoursOkayToCancel = 2;
+
     super.init(
       {
         date: Sequelize.STRING,
         canceled_at: Sequelize.STRING,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, hoursOkayToCancel));
+          },
+        },
       },
       {
         sequelize,

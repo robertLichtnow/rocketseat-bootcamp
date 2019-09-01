c2fd65fc3a774d3db1c0cf41fc4f77f57351d09f
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-31 21:40:44 -0300

    Added react component as class

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
-    techs: ['Node.js', 'ReactJS', 'React Native']
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+    newTech: ''
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      newTech: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
   };
 
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map((tech, index) => (
+            <li key={index}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }

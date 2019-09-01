156e4317d2c9c39dfca8ed41967a657e57dc326c
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-08-31 22:10:03 -0300

    Finished module

--- modulo04/src/components/TechItem.js
+++ modulo04/src/components/TechItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TechItem({ index, tech, onDelete }) {
   return (
@@ -10,5 +11,14 @@ function TechItem({ index, tech, onDelete }) {
     </li>
   );
 }
+
+TechItem.defaultProps = {
+  tech: 'Oculto'
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired
+};
 
 export default TechItem;

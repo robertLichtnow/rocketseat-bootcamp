8c1761427b1a27f3dd12235c227bef354dcf9e36
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-09-04 20:34:06 -0300

    Added new RN project

---
+++ modulo06/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
+      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
+      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
+      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
+      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: Colors.green,
+  },
+});
+
+export default App;

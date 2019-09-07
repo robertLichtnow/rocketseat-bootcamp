a7acb0195be50cb3d29594d8e78c139e70aa1713
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-09-07 10:09:43 -0300

    Adding navigation and gesture handler

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,36 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: Colors.green,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
-      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
-      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
-      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
-      <Text style={styles.sectionTitle}>Welcome to React Native</Text>
-    </View>
-  );
+  return <Routes />;
 };
 
 export default App;

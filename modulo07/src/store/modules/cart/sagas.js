627aefdccbba00928e268e0bd677cfeaf50f6154
Matheus Lichtnow <matheusrobert8@gmail.com>
2019-09-13 14:19:14 -0300

    Added navigation to saga

--- modulo07/src/store/modules/cart/sagas.js
+++ modulo07/src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import { formatPrice } from '../../../util/format';
+import history from '../../../services/history';
 import api from '../../../services/api';
 
 import { addToCartSuccess, updateAmountSuccess } from './actions';
@@ -34,6 +35,7 @@ function* addToCart({ id }) {
       priceFormatted: formatPrice(response.data.price),
     };
     yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
 }
 
